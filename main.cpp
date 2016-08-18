477de8aa79bc57e0af9e859edcdd2f23f44bb490
Yury.Alexandrov <Yury.Alexandrov@billing.ru>
2016-08-18 18:39:52 +0300

    half of monoid

--- main.cpp
+++ main.cpp
@@ -67,6 +67,31 @@ typename tuple<Ts&...> tie(Ts&... ts) {
 	return tuple<Ts&...>(ts...);
 }
 
+template<typename T>
+T add(const T& arg1, const T& arg2) {
+	return arg1 + arg2;
+}
+
+template<typename T, T _append(const T&, const T&) = add>
+struct monoid {
+	static const bool defined = false;
+	typedef T type;
+	static const T zero = T(0);
+
+	static T append(const T& arg1, const T& arg2) {
+		return _append(arg1, arg2);
+	}
+
+	static T pow(const T& arg, uint power) {
+		return arg * power;
+	}
+};
+
+template<typename T, T _append(const T&, const T&) = add, typename = std::enable_if<monoid<T, _append>::defined>::type>
+struct use {
+	typedef monoid<T, _append> m;
+	// use m::zero and shit
+};
 
 int main() {
 	tuple<int, double, char> c(1, 1.0, 'b');
@@ -74,5 +99,9 @@ int main() {
 	double b;
 	char g;
 	tie(a, b, g) = c;
+	//tuple<double, char> ee = c;
+	int q = monoid<int>::pow(10, 3);
+	int w = monoid<int>::append(10, 3);
+	use<int, add, int> t;
 	return 0;
 }
