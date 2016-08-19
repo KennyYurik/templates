af7d1b0d4ab36fa241c6b302bb4056040c5ffc6f
Yury.Alexandrov <Yury.Alexandrov@billing.ru>
2016-08-19 12:13:38 +0300

    monoid

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <vector>
+#include <algorithm>
+#include <limits>
 
 typedef unsigned int uint;
 
@@ -67,19 +69,17 @@ typename tuple<Ts&...> tie(Ts&... ts) {
 	return tuple<Ts&...>(ts...);
 }
 
-template<typename T>
-T add(const T& arg1, const T& arg2) {
-	return arg1 + arg2;
-}
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
 
-template<typename T, T _append(const T&, const T&) = add>
-struct monoid {
-	static const bool defined = false;
+template<typename T>
+struct monoid_add {
 	typedef T type;
 	static const T zero = T(0);
 
-	static T append(const T& arg1, const T& arg2) {
-		return _append(arg1, arg2);
+	static T append(const T& a, const T& b) {
+		return a + b;
 	}
 
 	static T pow(const T& arg, uint power) {
@@ -87,21 +87,27 @@ struct monoid {
 	}
 };
 
-template<typename T, T _append(const T&, const T&) = add, typename = std::enable_if<monoid<T, _append>::defined>::type>
-struct use {
-	typedef monoid<T, _append> m;
-	// use m::zero and shit
+template<typename T>
+using monoid = monoid_add<T>;
+
+template<typename T>
+struct monoid_min {
+	typedef T type;
+	static const T zero;
+
+	static T append(const T& a, const T& b) {
+		return std::min(a, b);
+	}
+
+	static T pow(const T& arg, uint power) {
+		return (power == 0) ? zero : arg;
+	}
 };
 
+template<typename T>
+const T monoid_min<T>::zero = std::numeric_limits<T>::max();
+
 int main() {
-	tuple<int, double, char> c(1, 1.0, 'b');
-	int a;
-	double b;
-	char g;
-	tie(a, b, g) = c;
-	//tuple<double, char> ee = c;
 	int q = monoid<int>::pow(10, 3);
-	int w = monoid<int>::append(10, 3);
-	use<int, add, int> t;
-	return 0;
+	double d = monoid_min<double>::zero;
 }
